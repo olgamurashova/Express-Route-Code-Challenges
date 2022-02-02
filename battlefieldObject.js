6f2efa3b4960428571aad1a969228997f0b893d6
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-02 01:24:02 -0500

    Create battlefieldObject.js

---
+++ battlefieldObject.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+const battlefields = {
+  fortSumter: {
+    state: 'SC',
+  },
+  manassas: {
+    state: 'VA',
+  },
+  gettysburg: {
+    state: 'PA',
+  },
+  antietam: {
+    state: 'MD',
+  }
+}
+
+app.get('/battlefields/:name', (req, res, next) => {
+  const battleField = battlefields[req.params.name];
+  if(battleField) {
+    res.send(battleField);
+  } else {
+    res.status(404).send();
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/battlefields/:name', (req, res, next) => {
+  const battlefieldName = req.params.name;
+  
+});
